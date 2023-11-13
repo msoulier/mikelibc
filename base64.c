fb9da209656f99cf3b736f124194b86cbe07ca07
Michael P. Soulier <msoulier@digitaltorque.ca>
2023-11-13 08:58:29 -0500

    Handling padding properly

--- base64.c
+++ base64.c
@@ -1,3 +1,5 @@
+#include <assert.h>
+
 #include "base64.h"
 
 // for main
@@ -9,11 +11,16 @@ unsigned char *base64_encode(const unsigned char *data,
 {
     ORIG_OCTETS orig;
     B64_BLOCK b64 = {0, 0, 0, 0};
+    int output_bytes = 4*((input_size+2)/3);
+    unsigned char *output = (unsigned char *)malloc(output_bytes+1);
+    assert( output != NULL );
     int i = 0;
+    int j = 0;
     while (i < input_size) {
         // Need 3 bytes as input.
         int remaining = input_size - i;
         printf("remaining is %d bytes\n", remaining);
+        int padding = 0;
         if (remaining >= 3) {
             orig.one = data[i];
             orig.two = data[i+1];
@@ -22,23 +29,37 @@ unsigned char *base64_encode(const unsigned char *data,
             orig.one = data[i];
             orig.two = data[i+1];
             orig.three = 0;
+            padding = 1;
         } else {
             orig.one = data[i];
             orig.two = 0;
             orig.three = 0;
+            padding = 2;
+        }
+        printf("orig size %ld, b64 size %ld\n", sizeof(ORIG_OCTETS),
+                                                sizeof(B64_BLOCK));
+        printf("orig: %c %c %c\n", orig.one, orig.two, orig.three);
+        memcpy(&b64, &orig, sizeof(orig));
+        printf("b64 raw: %d %d %d %d\n", b64.one, b64.two, b64.three, b64.four);
+        printf("b64: %c %c %c %c\n", b64_alphabet[b64.one],
+                                    b64_alphabet[b64.two],
+                                    b64_alphabet[b64.three],
+                                    b64_alphabet[b64.four]);
+        output[j] = b64_alphabet[b64.one];
+        output[j+1] = b64_alphabet[b64.two];
+        output[j+2] = b64_alphabet[b64.three];
+        output[j+3] = b64_alphabet[b64.four];
+        if (padding == 1) {
+            output[j+3] = '=';
+        } else if (padding == 2) {
+            output[j+2] = '=';
+            output[j+3] = '=';
         }
         i += 3;
+        j += 4;
     }
-    printf("orig size %ld, b64 size %ld\n", sizeof(ORIG_OCTETS),
-                                            sizeof(B64_BLOCK));
-    printf("orig: %c %c %c\n", orig.one, orig.two, orig.three);
-    memcpy(&b64, &orig, sizeof(orig));
-    printf("b64 raw: %d %d %d %d\n", b64.one, b64.two, b64.three, b64.four);
-    printf("b64: %c %c %c %c\n", b64_alphabet[b64.one],
-                                 b64_alphabet[b64.two],
-                                 b64_alphabet[b64.three],
-                                 b64_alphabet[b64.four]);
-    return NULL;
+    output[j] = '\0';
+    return output;
 }
 
 // for testing
